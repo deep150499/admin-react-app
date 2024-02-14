20d514dabd898e2a867cacbe2d9e314f6fd20c52
“deep150499” <deepnanavati@icloud.com>
2024-02-14 16:56:21 -0500

    Added dummy data

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { Link , NavLink} from 'react-router-dom';
+import { SiShopware } from 'react-icons/si';
+import { MdOutlineCancel } from 'react-icons/md'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import { links } from '../data/dummy'
 
 const Sidebar = () => {
   return (
